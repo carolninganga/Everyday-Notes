79cad5e7394b214524ea7ba4280bc9076d95291a
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-13 14:39:25 -0400

    defined schema in notes file

--- models/notes.js
+++ models/notes.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+
+const Schema = mongoose.Schema;
+const notesSchema = new Schema({
+    id: {
+        type: String,
+    },
+    title: {
+        type: String,
+        required: "Enter title for note"
+    },
+    text: {
+        type: String,
+        required:"Enter note"
+    }
+});
