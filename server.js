f07ba734f01a0df81a2da7d5540b232fc6f49998
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-13 15:02:22 -0400

    added mongoose connection

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var path = require("path");
 var app = express();
+
+// require mongoose and install as dependency
+const mongoose = require("mongoose");
+
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//add connection to db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/notesapp", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 
 require("./routes/apiRoutes.js")(app);
