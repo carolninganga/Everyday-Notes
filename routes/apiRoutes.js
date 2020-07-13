1e55e29b107877762b7f7a12826cbeded9ecf56b
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-13 14:51:18 -0400

    added a post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,29 +29,38 @@ module.exports = function (app) {
   })
   
 
+  //Delete request to remove the note from the database
+  app.delete("/api/notes/:id", function (req, res) { 
+    Notes.deleteOne({_id: mongodb.ObjectID( req.params.id)}, (err, result) => {
+       if (err) return console.log(err)
+       console.log(req.body)
+       res.redirect('/')
+     })
+    })
+
   
 
      //this handles the delete req when submits the form to the server
 
-     app.delete("/api/notes/:id", function (req, res) {
+    //  app.delete("/api/notes/:id", function (req, res) {
 
-        console.log("next", req.params)
-        var noteId = req.params.id
-        dataNote = dataNote.filter(x => x.id != noteId)
+    //     console.log("next", req.params)
+    //     var noteId = req.params.id
+    //     dataNote = dataNote.filter(x => x.id != noteId)
   
-        // dataNote = []
-        // console.log(dataNote)  
-        // dataNote.filter(req.params.id);
+    //     // dataNote = []
+    //     // console.log(dataNote)  
+    //     // dataNote.filter(req.params.id);
   
-        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(dataNote), function (
-          err
-        ) {
-          if (err) throw err;
-          console.log('Filtered note saved')
-        });
+    //     fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(dataNote), function (
+    //       err
+    //     ) {
+    //       if (err) throw err;
+    //       console.log('Filtered note saved')
+    //     });
   
-        res.json(true);
-      });
+    //     res.json(true);
+    //   });
   
       app.get("/", function (req, res) {
         //res.sendFile(path.join(__dirname, "../public/index.html"));
